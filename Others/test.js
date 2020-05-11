4c46ffb8de37a98f50f9c19b023f14ebf6991abb
Barton NIU <barton.niu@gmail.com>
2020-05-11 17:17:29 +1000

    Add a new Q about finding same values in two arrays

---
+++ Others/test.js
@@ -0,0 +1,11 @@
+var function1 = function () {
+    console.log("Hello!");
+};
+
+
+
+function function2() {
+    console.log("Hello!");
+}
+console.log(function1.toString());
+console.log(function2.toString());
